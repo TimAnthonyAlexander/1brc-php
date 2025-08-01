8cc0ffe9116210860000b56964ab3ec931f68e98
Tim Anthony Alexander <info@timanthonyalexander.de>
2025-08-01 17:39:08 +0200

    Start of php 1brc

---
+++ scripts/main.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace scripts;
+
+use TimAlexander\OnebrcPhp\Main;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$main = new Main();
+$main->run();
---
+++ src/Main.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TimAlexander\OnebrcPhp;
+
+final class Main
+{
+    public function run(): void
+    {
+        $file = __DIR__ . '/../measurements-1000000000.txt';
+    }
+}
